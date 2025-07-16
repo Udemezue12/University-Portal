c0f572d280db49b36d659d586384f908dd1ce4b9
Uchechukwu Udemezue <udemezue008@gmail.com>
2025-07-16 18:13:32 +0200

    Made major modifications

--- frontend/src/components/api_route/api.js
+++ frontend/src/components/api_route/api.js
@@ -1,4 +1,12 @@
-export const API_URL = process.env.REACT_APP_API_URL;
-export const WEBSOCKET_API_URL = process.env.REACT_APP_API_URL_FOR_WEBSOCKET;
-export const RP_ID = process.env.REACT_APP_RP_ID
-export const Name = process.env.REACT_APP_NAME
+// export const API_URL = process.env.REACT_APP_API_URL;
+// = process.env.REACT_APP_API_URL_FOR_WEBSOCKET;
+
+export const API_URL = https://crm-1uxm.onrender.com;
+export const WEBSOCKET_API_URL = ws:https://crm-1uxm.onrender.com;
+
+export const RP_ID = crm-1uxm.onrender.com;
+export const Name = "Astro";
+
+// export const WEBSOCKET_API_URL = process.env.REACT_APP_API_URL_FOR_WEBSOCKET;
+// export const RP_ID = process.env.REACT_APP_RP_ID
+// export const Name = process.env.REACT_APP_NAME
