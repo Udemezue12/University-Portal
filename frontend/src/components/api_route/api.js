0d6a6bbbd0390bb5384adf08162bb08c942f3f7e
Uchechukwu Udemezue <udemezue008@gmail.com>
2025-07-17 11:40:50 +0200

    Slight modifications were made

--- frontend/src/components/api_route/api.js
+++ frontend/src/components/api_route/api.js
@@ -1,10 +1,11 @@
 // export const API_URL = process.env.REACT_APP_API_URL;
 
 
-export const API_URL = "https://crm-1uxm.onrender.com";
-export const WEBSOCKET_API_URL = "wss://crm-1uxm.onrender.com";
-
-export const RP_ID = "crm-1uxm.onrender.com";
+// export const API_URL = "https://crm-1uxm.onrender.com";
+export const WEBSOCKET_API_URL = "wss:university-portal-g6a1x.sevalla.app/";
+export const RP_ID= "university-portal-g6a1x.sevalla.app/"
+export const API_URL = "https://university-portal-g6a1x.sevalla.app/"
+// export const RP_ID = "crm-1uxm.onrender.com";
 export const Name = "Astro";
 
 // export const WEBSOCKET_API_URL = process.env.REACT_APP_API_URL_FOR_WEBSOCKET;
