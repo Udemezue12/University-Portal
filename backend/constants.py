cd4fc0ac6230ca45fac9d76a547e330e46f28fa8
Astro Designs <97709926+Udemezue12@users.noreply.github.com>
2025-07-18 20:36:44 +0200

    Update constants.py
    Deleted some redundant codes

--- backend/constants.py
+++ backend/constants.py
@@ -12,14 +12,7 @@ from database import get_db_async
 from model import User
 
 
-# def save_uploaded_file(file: UploadFile) -> str:
-#     if not file.filename.lower().endswith(('.pdf', '.docx')):
-#         raise HTTPException(
-#             status_code=400, detail="Only PDF or DOCX Files are allowed")
-#     file_path = os.path.join(UPLOAD_DIR, file.filename)
-#     with open(file_path, 'wb') as f:
-#         shutil.copyfileobj(file.file, f)
-#     return file_path
+
 async def save_uploaded_file(file: UploadFile) -> str:
     """
     Asynchronously saves a PDF or DOCX upload.
@@ -39,14 +32,7 @@ async def save_uploaded_file(file: UploadFile) -> str:
 
     return file_path
 
-# def get_current_user(
-#     user_id: str = Depends(jwt_protect),
-#     db: Session = Depends(get_db)
-# ):
-#     user = db.query(User).filter(User.id == user_id).first()
-    # if not user:
-    #     raise HTTPException(status_code=404, detail="User not found")
-    # return user
+
 
 
 async def get_current_user(user_id: str = Depends(jwt_protect), db: AsyncSession = Depends(get_db_async)):
@@ -65,11 +51,3 @@ async def passkey_get_current_user(user_id: str = Depends(jwt_protect), db: Asyn
     return user_result
 
 
-# def passkey_get_current_user(
-#     user_id: int = Depends(passkey_jwt_protect),
-#     db: Session = Depends(get_db)
-# ) -> User:
-#     user = db.query(User).filter(User.id == user_id).first()
-#     if not user:
-#         raise HTTPException(status_code=404, detail="User not found")
-#     return user
