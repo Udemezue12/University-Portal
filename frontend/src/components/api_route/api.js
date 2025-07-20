b7b50aff1793fd0bc0b5ed630eb16f55f52aa841
Astro Designs <97709926+Udemezue12@users.noreply.github.com>
2025-07-21 01:35:25 +0200

    Update api.js
    Made some changes

--- frontend/src/components/api_route/api.js
+++ frontend/src/components/api_route/api.js
@@ -1,13 +1,15 @@
 // export const API_URL = process.env.REACT_APP_API_URL;
 
-
-// export const API_URL = "https://crm-1uxm.onrender.com";
-export const WEBSOCKET_API_URL = "wss:university-portal-g6a1x.sevalla.app/";
-export const RP_ID= "university-portal-g6a1x.sevalla.app/"
-export const API_URL = "https://university-portal-g6a1x.sevalla.app/"
-// export const RP_ID = "crm-1uxm.onrender.com";
-export const Name = "Astro";
-
 // export const WEBSOCKET_API_URL = process.env.REACT_APP_API_URL_FOR_WEBSOCKET;
 // export const RP_ID = process.env.REACT_APP_RP_ID
 // export const Name = process.env.REACT_APP_NAME
+
+export const API_URL = "https://crm-1uxm.onrender.com";
+export const RP_ID = "crm-1uxm.onrender.com";
+export const Name = "Astro";
+export const WEBSOCKET_API_URL = "wss:crm-1uxm.onrender.com";
+// /
+// export const WEBSOCKET_API_URL = "wss:university-portal-g6a1x.sevalla.app/";
+// export const RP_ID= "university-portal-g6a1x.sevalla.app/"
+// export const API_URL = "https://university-portal-g6a1x.sevalla.app/"
+// export const Name = "Astro";
